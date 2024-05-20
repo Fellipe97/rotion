de82a00367738481ca7943bbdf7aee3f91ac698e
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-20 09:49:57 -0300

    feat: :lipstick: padrão de cores pro app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,8 +5,8 @@ export function App() {
 
 
   return (
-    <>
+    <div className='h-screen w-screen text-rotion-100'>
       <h1 className='text-violet-400 text-4xl' >Hello, World!</h1>
-    </>
+    </div>
   )
 }
