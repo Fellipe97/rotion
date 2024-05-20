82490954fd324bb57937843e1d58411eaaa2bbb5
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-20 10:45:05 -0300

    feat: :lipstick: criação dos componentes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,22 @@
 import './styles/global.css'
 
+import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
+
 
 export function App() {
 
 
   return (
-    <div className='h-screen w-screen text-rotion-100'>
-      <h1 className='text-violet-400 text-4xl' >Hello, World!</h1>
+    <div className='h-screen w-screen text-rotion-100 flex'>
+      <Sidebar />
+      <div className='flex-1 flex flex-col max-h-screen'>
+        <Header />
+
+        <main className='flex-1 flex items-center justify-center text-rotion-400'>
+          Selecione ou crie um documento
+        </main>
+      </div>
     </div>
   )
 }
