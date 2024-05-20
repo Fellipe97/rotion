e148030c93973c7b7cc3e501e28777efd4c32324
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-20 12:15:47 -0300

    feat: :sparkles: navegação entre telas

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join, resolve } from 'node:path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png'
+import { join, resolve } from 'node:path'
+import { createFileRoute, createURLRoute } from 'electron-router-dom'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 
 function createWindow(): void {
   // Create the browser window.
@@ -32,12 +33,19 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
+  const devServerURL = createURLRoute(process.env['ELECTRON_RENDERER_URL']!, 'main')
+
+  const fileRoute = createFileRoute(
+    join(__dirname, '../renderer/index.html'),
+    'main'
+  )
+
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(devServerURL)
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(...fileRoute)
   }
 }
 
