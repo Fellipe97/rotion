cc756c9086ee0c87d1b8968dd84998730fec87a2
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-20 11:27:15 -0300

    feat: :sparkles: utilização de drag

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
@@ -21,9 +23,22 @@ module.exports = {
           900: '#17141f',
         },
       },
-      
+
     },
   },
-  plugins: [],
+
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.region-drag': {
+          '-webkit-app-region': 'drag',
+        },
+        '.region-no-drag': {
+          '-webkit-app-region': 'no-drag',
+        }
+      })
+    })
+  ],
+
 }
 
