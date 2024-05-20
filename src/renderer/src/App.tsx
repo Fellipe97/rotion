842167ac85242be4d4e00f5c3abcb697778147a7
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-20 12:21:28 -0300

    feat: :sparkles: configurando layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,15 +6,5 @@ import { Routes } from './Routes'
 
 
 export function App() {
-
-
-  return (
-    <div className='h-screen w-screen text-rotion-100 flex'>
-      <Sidebar />
-      <div className='flex-1 flex flex-col max-h-screen'>
-        <Header />
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
