9e2d1d0b33f288364d4726f567f086b08c140573
Fellipe97 <lfalmeida97@hotmail.com>
2024-05-29 17:40:45 -0300

    update

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 
 export function Document() {
     return (
-        <main className='flex-1 flex items-center justify-center text-rotion-400'>
-            Document
-            <Link to="/">Acessar blank</Link>
+        <main className='flex-1 flex py-12 px-18 gap-8'>
+            <aside className="hidden lg:block sticky top-0">
+                <span className="text-rotion-300 font-semibold text-xs">
+                    TABLE OF CONTENT
+                </span>
+            </aside>
         </main>
     )
 }
