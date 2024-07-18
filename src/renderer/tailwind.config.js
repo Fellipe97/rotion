ef029ed1be437f7f4021fe803764c96f36900e2c
Fellipe97 <lfalmeida97@hotmail.com>
2024-07-18 16:55:32 -0300

    feat: :sparkles: busca na aplicação

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -47,6 +47,8 @@ module.exports = {
 
   plugins: [
     require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
+
     plugin(({ addUtilities }) => {
       addUtilities({
         '.region-drag': {
